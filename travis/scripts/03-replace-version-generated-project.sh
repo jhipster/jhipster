fea03c93c6de7e9ebe567ee8e15aaec714fab002
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-03-16 20:34:30 +0100

    Travis: cat and grep to check the pom.xml or gradle.properties

--- travis/scripts/03-replace-version-generated-project.sh
+++ travis/scripts/03-replace-version-generated-project.sh
@@ -7,7 +7,10 @@ fi
 # jhipster-dependencies.version in generated pom.xml or gradle.properties
 if [[ -a mvnw ]]; then
     sed -i 's/<jhipster-dependencies.version>.*<\/jhipster-dependencies.version>/<jhipster-dependencies.version>'$VERSION'<\/jhipster-dependencies.version>/1' pom.xml
+    cat pom.xml | grep \<jhipster-dependencies.version\>
 
 elif [[ -a gradlew ]]; then
     sed -i 's/jhipster_dependencies_version=.*/jhipster_dependencies_version='$VERSION'/1' gradle.properties
+    cat gradle.properties | grep jhipster_dependencies_version=
+
 fi
