3a72aebe074a98ef65db046cf1892f6e590c0419
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-03-16 20:24:04 +0100

    Rework Travis builds

---
+++ travis/scripts/03-replace-version-generated-project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $VERSION == '' ]]
+then
+  VERSION=0.0.0-TRAVIS
+fi
+
+# jhipster-dependencies.version in generated pom.xml or gradle.properties
+if [[ -a mvnw ]]; then
+  sed -i 's/<jhipster-dependencies.version>.*<\/jhipster-dependencies.version>/<jhipster-dependencies.version>'$VERSION'<\/jhipster-dependencies.version>/1' pom.xml
+elif [[ -a gradlew ]]; then
+  sed -i 's/jhipster_dependencies_version=.*/jhipster_dependencies_version='$VERSION'/1' gradle.properties
+fi
