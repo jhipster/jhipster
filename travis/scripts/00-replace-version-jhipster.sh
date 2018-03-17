973528f5f5b5f521ee52aa26aa66378eda659a57
Erik Kemperman <erikkemperman@gmail.com>
2018-03-17 16:46:20 +0100

    On macOS, sed is a bit more strict about syntax and flags (#1)

--- travis/scripts/00-replace-version-jhipster.sh
+++ travis/scripts/00-replace-version-jhipster.sh
@@ -5,16 +5,21 @@ if [[ $JHIPSTER_VERSION == '' ]]; then
 fi
 
 # artifact version of jhipster-parent
-sed -i '/<artifactId>jhipster-parent<\/artifactId>/{$!{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1}}' pom.xml
+sed -e '/<artifactId>jhipster-parent<\/artifactId>/{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1;}' pom.xml > pom.xml.sed
+mv -f pom.xml.sed pom.xml
 
 # artifact version of jhipster-dependencies
-sed -i '/<artifactId>jhipster-dependencies<\/artifactId>/{$!{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1}}' jhipster-dependencies/pom.xml
+sed -e '/<artifactId>jhipster-dependencies<\/artifactId>/{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1;}' jhipster-dependencies/pom.xml > jhipster-dependencies/pom.xml.sed
+mv -f jhipster-dependencies/pom.xml.sed jhipster-dependencies/pom.xml
 
 # jhipster-server.version property in jhipster-dependencies
-sed -i 's/<jhipster-server.version>.*<\/jhipster-server.version>/<jhipster-server.version>'$JHIPSTER_VERSION'<\/jhipster-server.version>/1' jhipster-dependencies/pom.xml
+sed -e 's/<jhipster-server.version>.*<\/jhipster-server.version>/<jhipster-server.version>'$JHIPSTER_VERSION'<\/jhipster-server.version>/1' jhipster-dependencies/pom.xml > jhipster-dependencies/pom.xml.sed
+mv -f jhipster-dependencies/pom.xml.sed jhipster-dependencies/pom.xml
 
 # parent version of jhipster-server
-sed -i '/<artifactId>jhipster-dependencies<\/artifactId>/{$!{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1}}' jhipster-server/pom.xml
+sed -e '/<artifactId>jhipster-dependencies<\/artifactId>/{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1;}' jhipster-server/pom.xml > jhipster-server/pom.xml.sed
+mv -f jhipster-server/pom.xml.sed jhipster-server/pom.xml
 
 # artifact version of jhipster-server
-sed -i '/<artifactId>jhipster<\/artifactId>/{$!{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1}}' jhipster-server/pom.xml
+sed -e '/<artifactId>jhipster<\/artifactId>/{N;s/<version>.*<\/version>/<version>'$JHIPSTER_VERSION'<\/version>/1;}' jhipster-server/pom.xml > jhipster-server/pom.xml.sed
+mv -f jhipster-server/pom.xml.sed jhipster-server/pom.xml
--- travis/scripts/03-replace-version-generated-project.sh
+++ travis/scripts/03-replace-version-generated-project.sh
@@ -7,11 +7,13 @@ fi
 # jhipster-dependencies.version in generated pom.xml or gradle.properties
 cd "$APP_FOLDER"
 if [[ -a mvnw ]]; then
-    sed -i 's/<jhipster-dependencies.version>.*<\/jhipster-dependencies.version>/<jhipster-dependencies.version>'$VERSION'<\/jhipster-dependencies.version>/1' pom.xml
+    sed -e 's/<jhipster-dependencies.version>.*<\/jhipster-dependencies.version>/<jhipster-dependencies.version>'$VERSION'<\/jhipster-dependencies.version>/1;' pom.xml > pom.xml.sed
+    mv -f pom.xml.sed pom.xml
     cat pom.xml | grep \<jhipster-dependencies.version\>
 
 elif [[ -a gradlew ]]; then
-    sed -i 's/jhipster_dependencies_version=.*/jhipster_dependencies_version='$VERSION'/1' gradle.properties
+    sed -e 's/jhipster_dependencies_version=.*/jhipster_dependencies_version='$VERSION'/1;' gradle.properties > gradle.properties.sed
+    mv -f gradle.properties.sed gradle.properties
     cat gradle.properties | grep jhipster_dependencies_version=
 
 fi
