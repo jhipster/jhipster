de57cb78fc749fb9fb919d13f497639aa3929e91
Erik Kemperman <erikkemperman@gmail.com>
2018-04-11 10:51:15 +0200

    Fix Travis script

--- travis/scripts/00-install-jhipster.sh
+++ travis/scripts/00-install-jhipster.sh
@@ -19,7 +19,7 @@ if [[ "$TRAVIS_REPO_SLUG" == *"/jhipster" ]]; then
     travis/scripts/00-replace-version-jhipster.sh
 
     ./mvnw clean install -Dgpg.skip=true
-    ls -al ~/.m2/repository/io/github/jhipster/jhipster/
+    ls -al ~/.m2/repository/io/github/jhipster/jhipster-framework/
     ls -al ~/.m2/repository/io/github/jhipster/jhipster-dependencies/
     ls -al ~/.m2/repository/io/github/jhipster/jhipster-parent/
 
@@ -38,7 +38,7 @@ else
     git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
     ./mvnw clean install -Dgpg.skip=true
-    ls -al ~/.m2/repository/io/github/jhipster/jhipster/
+    ls -al ~/.m2/repository/io/github/jhipster/jhipster-framework/
     ls -al ~/.m2/repository/io/github/jhipster/jhipster-dependencies/
     ls -al ~/.m2/repository/io/github/jhipster/jhipster-parent/
 fi
