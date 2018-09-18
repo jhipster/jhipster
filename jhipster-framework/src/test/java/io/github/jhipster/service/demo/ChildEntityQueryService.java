7132c6ffdc79b2211b81103266aebb8906397813
Zsombor Gegesy <gzsombor@gmail.com>
2018-09-18 21:12:39 +0200

    Followup for  #8040 - adding a 'compile test' for future reference - if it compiles, the query service is ok

---
+++ jhipster-framework/src/test/java/io/github/jhipster/service/demo/ChildEntityQueryService.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * This file is part of the JHipster project, see https://www.jhipster.tech/
+ * for more information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.jhipster.service.demo;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import io.github.jhipster.service.QueryService;
+import io.github.jhipster.service.filter.LongFilter;
+
+/**
+ * This class is just a compile - test.
+ *
+ */
+public class ChildEntityQueryService extends QueryService<ChildEntity> {
+
+    static class ChildEntityCriteria extends BaseEntityQueryService.BaseEntityCriteria {
+        LongFilter parentId;
+        
+        public LongFilter getParentId() {
+            return id;
+        }
+    }
+
+    public Specification<ChildEntity> createSpecification(ChildEntityCriteria criteria) {
+        Specification<ChildEntity> specification = Specification.where(null);
+        if (criteria.getParentId() != null) {
+            specification = specification.and(buildReferringEntitySpecification(criteria.getParentId(), ChildEntity_.parent, ParentEntity_.id)); 
+        }
+        return specification;
+    }
+    
+}
