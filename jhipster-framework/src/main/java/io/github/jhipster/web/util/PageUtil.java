365df3dca88b683c5ba38d87bfee2a8c61e34de9
Julien Dubois <julien.dubois@gmail.com>
2019-02-11 14:26:45 +0100

    clean up code

--- jhipster-framework/src/main/java/io/github/jhipster/web/util/PageUtil.java
+++ jhipster-framework/src/main/java/io/github/jhipster/web/util/PageUtil.java
@@ -1,6 +1,5 @@
 package io.github.jhipster.web.util;
 
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -9,10 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Utility class for creating a Page
+ * Utility class for creating a Page object.
  */
 public interface PageUtil {
-
 
     /**
      * Create a {@link Page} from a {@link List} of objects
@@ -23,7 +21,7 @@ public interface PageUtil {
      * @return page containing objects, and attributes set according to pageable
      * @throws IllegalArgumentException - if list is null
      */
-    public static <T> Page<T> createPageFromList(List<T> list, Pageable pageable) {
+    static <T> Page<T> createPageFromList(List<T> list, Pageable pageable) {
         if (list == null) {
             throw new IllegalArgumentException("To create a Page, the list mustn't be null!");
         }
