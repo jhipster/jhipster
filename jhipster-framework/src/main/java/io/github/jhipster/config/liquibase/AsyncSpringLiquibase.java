aa63fef46e189b2ad0086c03fe09c5d19b3b40e0
DanielFran <dandrfranco@gmail.com>
2019-07-28 16:55:00 +0100

    AsyncSpringLiquibase now extends from DataSourceClosingSpringLiquibase to allow closing new specific DataSource exclusivelly used for liquibase process

--- jhipster-framework/src/main/java/io/github/jhipster/config/liquibase/AsyncSpringLiquibase.java
+++ jhipster-framework/src/main/java/io/github/jhipster/config/liquibase/AsyncSpringLiquibase.java
@@ -16,13 +16,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.github.jhipster.config.liquibase;
 
 import liquibase.exception.LiquibaseException;
-import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.liquibase.DataSourceClosingSpringLiquibase;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.util.StopWatch;
@@ -34,15 +33,15 @@ import java.util.concurrent.Executor;
 import static io.github.jhipster.config.JHipsterConstants.*;
 
 /**
- * Specific liquibase.integration.spring.SpringLiquibase that will update the database asynchronously. <p> By default,
- * this asynchronous version only works when using the "dev" profile.<p> The standard
+ * Specific liquibase.integration.spring.SpringLiquibase that will update the database asynchronously and close 
+ * DataSource if necessary. <p> By default, this asynchronous version only works when using the "dev" profile.<p> The standard
  * liquibase.integration.spring.SpringLiquibase starts Liquibase in the current thread: <ul> <li>This is needed if you
  * want to do some database requests at startup</li> <li>This ensure that the database is ready when the application
  * starts</li> </ul> But as this is a rather slow process, we use this asynchronous version to speed up our start-up
  * time: <ul> <li>On a recent MacBook Pro, start-up time is down from 14 seconds to 8 seconds</li> <li>In production,
  * this can help your application run on platforms like Heroku, where it must start/restart very quickly</li> </ul>
  */
-public class AsyncSpringLiquibase extends SpringLiquibase {
+public class AsyncSpringLiquibase extends DataSourceClosingSpringLiquibase {
 
     /** Constant <code>DISABLED_MESSAGE="Liquibase is disabled"</code> */
     public static final String DISABLED_MESSAGE = "Liquibase is disabled";
