aa4a20c99f7d6e15fa1b3155d3b9beb04262a268
jamshid <jamshidrostami@gmail.com>
2020-04-18 14:20:36 +0430

    fix some tests issues

--- jhipster-framework/src/main/java/io/github/jhipster/service/filter/RangeFilter.java
+++ jhipster-framework/src/main/java/io/github/jhipster/service/filter/RangeFilter.java
@@ -228,7 +228,7 @@ public class RangeFilter<FIELD_TYPE extends Comparable<? super FIELD_TYPE>> exte
             + (getEquals() != null ? "equals=" + getEquals() + ", " : "")
             + (getNotEquals() != null ? "notEquals=" + getNotEquals() + ", " : "")
             + (getSpecified() != null ? "specified=" + getSpecified() + ", " : "")
-            + (getIn() != null ? "in=" + getIn() : "")
+            + (getIn() != null ? "in=" + getIn() + ", " : "")
             + (getNotIn() != null ? "notIn=" + getNotIn() : "")
             + "]";
     }
