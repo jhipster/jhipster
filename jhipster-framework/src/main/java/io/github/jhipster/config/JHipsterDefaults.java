a856429137596e8ca81762640fe99f3065fb1c9a
Frederik Hahne <atomfrede@gmail.com>
2020-06-03 18:10:20 +0200

    add more memcache and redis properties
    to control e.g. connection limits easier in environments with limited resources
    
    updates https://github.com/jhipster/generator-jhipster/issues/11695
    updates https://github.com/jhipster/generator-jhipster/issues/11696

--- jhipster-framework/src/main/java/io/github/jhipster/config/JHipsterDefaults.java
+++ jhipster-framework/src/main/java/io/github/jhipster/config/JHipsterDefaults.java
@@ -101,12 +101,20 @@ public interface JHipsterDefaults {
             String servers = "localhost:11211";
             int expiration = 300; // 5 minutes
             boolean useBinaryProtocol = true;
+
+            interface Authentication {
+                boolean enabled = false;
+            }
         }
 
         interface Redis {
             String[] server = {"redis://localhost:6379"};
             int expiration = 300; // 5 minutes
             boolean cluster = false;
+            int connectionPoolSize = 64; // default as in redisson
+            int connectionMinimumIdleSize = 24; // default as in redisson
+            int subscriptionConnectionPoolSize = 50; // default as in redisson
+            int subscriptionConnectionMinimumIdleSize = 1; // default as in redisson
         }
     }
 
